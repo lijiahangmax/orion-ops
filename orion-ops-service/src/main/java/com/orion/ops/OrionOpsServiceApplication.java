20ad062ed3c28d471def54120e8bda976c851ebd
lijiahang <ljh1553488six@139.com>
2021-03-24 16:07:54 +0800

    first commit

---
+++ orion-ops-service/src/main/java/com/orion/ops/OrionOpsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.orion.ops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrionOpsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrionOpsServiceApplication.class, args);
+    }
+
+}
