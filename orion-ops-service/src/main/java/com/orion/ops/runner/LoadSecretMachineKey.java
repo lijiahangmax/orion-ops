f912a7f4d923184a3820f0148cc0c0fba3182b12
lijiahang <ljh1553488six@139.com>
2021-04-18 21:41:01 +0800

    fixed: load local key notfound

--- orion-ops-service/src/main/java/com/orion/ops/runner/LoadSecretMachineKey.java
+++ orion-ops-service/src/main/java/com/orion/ops/runner/LoadSecretMachineKey.java
@@ -2,6 +2,7 @@ package com.orion.ops.runner;
 
 import com.orion.ops.dao.MachineSecretKeyDAO;
 import com.orion.ops.entity.domain.MachineSecretKeyDO;
+import com.orion.ops.utils.ValueMix;
 import com.orion.remote.channel.SessionHolder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
@@ -9,6 +10,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.io.File;
 import java.util.List;
 
 /**
@@ -32,10 +34,16 @@ public class LoadSecretMachineKey implements CommandLineRunner {
         List<MachineSecretKeyDO> keys = machineSecretKeyDAO.selectList(null);
         for (MachineSecretKeyDO key : keys) {
             String secretKeyPath = key.getSecretKeyPath();
+            File secretKey = new File(secretKeyPath);
+            if (!secretKey.exists() || !secretKey.isFile()) {
+                log.info("加载ssh私钥失败 未找到文件 {} {}", key.getKeyName(), secretKeyPath);
+                continue;
+            }
             String password = key.getPassword();
             log.info("加载ssh私钥 {} {}", key.getKeyName(), secretKeyPath);
             if (password != null) {
-                SessionHolder.addIdentity(secretKeyPath, password);
+                String decrypt = ValueMix.decrypt(password);
+                SessionHolder.addIdentity(secretKeyPath, decrypt);
             } else {
                 SessionHolder.addIdentity(secretKeyPath);
             }
