a70db4e8403efaa3877d4bd5f2966a3a444735c7
lijiahang <ljh1553488six@139.com>
2021-06-22 19:59:21 +0800

    add sftp api

---
+++ orion-ops-service/src/main/java/com/orion/ops/controller/SftpController.java
@@ -0,0 +1,29 @@
+package com.orion.ops.controller;
+
+import com.orion.ops.annotation.RestWrapper;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * sftp 操作
+ *
+ * @author Jiahang Li
+ * @version 1.0.0
+ * @since 2021/6/22 19:53
+ */
+@RestController
+@RestWrapper
+@RequestMapping("/orion/api/sftp")
+public class SftpController {
+
+    // ls 隐藏
+    // cd
+    // rm
+    // mv
+    // cat
+    // download
+    // upload
+    // touch
+    // mkdir
+
+}
