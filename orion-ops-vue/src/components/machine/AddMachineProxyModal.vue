0d8ba302d730dc14a27edf62f6dfefed4e389b9c
lijiahang <ljh1553488six@139.com>
2021-10-31 08:33:53 +0800

    add machine proxy.

---
+++ orion-ops-vue/src/components/machine/AddMachineProxyModal.vue
@@ -0,0 +1,192 @@
+<template>
+  <a-modal v-model="visible"
+           :title="title"
+           :width="450"
+           :okButtonProps="{props: {disabled: loading}}"
+           :mask="mask"
+           @ok="check"
+           @cancel="close">
+    <a-spin :spinning="loading">
+      <a-form :form="form" v-bind="layout">
+        <a-form-item label="代理主机" hasFeedback>
+          <a-input v-decorator="decorators.host"/>
+        </a-form-item>
+        <a-form-item label="代理端口" hasFeedback>
+          <a-input v-decorator="decorators.port"/>
+        </a-form-item>
+        <a-form-item label="代理用户">
+          <a-input v-decorator="decorators.username"/>
+        </a-form-item>
+        <a-form-item label="密码">
+          <a-input-password v-decorator="decorators.password"/>
+        </a-form-item>
+        <a-form-item label="代理类型">
+          <a-select v-decorator="decorators.type" placeholder="请选择">
+            <a-select-option :value="type.value" v-for="type in $enum.MACHINE_PROXY_TYPE" :key="type.value">
+              {{type.label}}
+            </a-select-option>
+          </a-select>
+        </a-form-item>
+        <a-form-item label="描述">
+          <a-textarea v-decorator="decorators.description"/>
+        </a-form-item>
+      </a-form>
+    </a-spin>
+  </a-modal>
+</template>
+
+<script>
+  import { pick } from 'lodash'
+  import { validatePort } from '../../lib/validate'
+
+  const layout = {
+    labelCol: { span: 5 },
+    wrapperCol: { span: 17 }
+  }
+
+  function getDecorators() {
+    return {
+      host: ['host', {
+        rules: [{
+          required: true,
+          message: '请输入主机'
+        }, {
+          max: 32,
+          message: '主机长度必须小于等于32位'
+        }]
+      }],
+      port: ['port', {
+        initialValue: 22,
+        rules: [{
+          required: true,
+          message: '请输入端口'
+        }, {
+          validator: validatePort
+        }]
+      }],
+      username: ['username', {
+        rules: [{
+          max: 128,
+          message: '用户名必须小于等于128位'
+        }, {
+          validator: this.validateUsername
+        }]
+      }],
+      password: ['password', {
+        rules: [{
+          max: 128,
+          message: '密码必须小于等于128位'
+        }, {
+          validator: this.validatePassword
+        }]
+      }],
+      type: ['type', {
+        rules: [{
+          required: true,
+          message: '请选择类型'
+        }]
+      }],
+      description: ['description', {
+        rules: [{
+          max: 64,
+          message: '描述必须小于等于64位'
+        }]
+      }]
+    }
+  }
+
+  export default {
+    name: 'AddMachineProxyModal',
+    data: function() {
+      return {
+        id: null,
+        visible: false,
+        title: null,
+        loading: false,
+        mask: true,
+        record: null,
+        layout: layout,
+        decorators: getDecorators.call(this),
+        form: this.$form.createForm(this)
+      }
+    },
+    methods: {
+      add() {
+        this.title = '新增代理'
+        this.initRecord({})
+      },
+      update(row) {
+        this.title = '修改代理'
+        this.initRecord(row)
+      },
+      initRecord(row) {
+        this.form.resetFields()
+        this.visible = true
+        this.id = row.id
+        this.record = pick(Object.assign({}, row), 'host', 'port', 'username', 'type', 'description')
+        this.$nextTick(() => {
+          this.form.setFieldsValue(this.record)
+        })
+      },
+      validateUsername(rule, value, callback) {
+        if (this.form.getFieldValue('password') && !value) {
+          callback(new Error('用户名和密码须同时存在'))
+        } else {
+          callback()
+        }
+      },
+      validatePassword(rule, value, callback) {
+        if (this.form.getFieldValue('username') && !value && !this.id) {
+          callback(new Error('新增时用户名和密码须同时存在'))
+        } else {
+          callback()
+        }
+      },
+      check() {
+        this.loading = true
+        this.form.validateFields((err, values) => {
+          if (err) {
+            this.loading = false
+            return
+          }
+          this.submit(values)
+        })
+      },
+      async submit(values) {
+        let res
+        try {
+          if (!this.id) {
+            // 添加
+            res = await this.$api.addMachineProxy({ ...values })
+          } else {
+            // 修改
+            res = await this.$api.updateMachineProxy({
+              ...values,
+              id: this.id
+            })
+          }
+          if (!this.id) {
+            this.$message.success('添加成功')
+            this.$emit('added', res.data)
+          } else {
+            this.$message.success('修改成功')
+            this.$emit('updated', res.data)
+          }
+          this.close()
+        } catch (e) {
+          // ignore
+        }
+        this.loading = false
+      },
+      close() {
+        this.id = null
+        this.visible = false
+        this.loading = false
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ orion-ops-vue/src/views/machine/MachineProxy.vue
@@ -0,0 +1,261 @@
+<template>
+  <div class="machine-proxy-container">
+    <!-- 搜索列 -->
+    <div class="machine-proxy-column">
+      <a-form-model class="machine-proxy-search-form" ref="query" :model="query">
+        <a-row>
+          <a-col :span="5">
+            <a-form-model-item label="代理主机" prop="host">
+              <a-input v-model="query.host"/>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="5">
+            <a-form-model-item label="代理端口" prop="port">
+              <a-input v-limit-integer v-model="query.port"/>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="5">
+            <a-form-model-item label="代理用户" prop="username">
+              <a-input v-model="query.username"/>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="5">
+            <a-form-model-item label="代理类型" prop="type">
+              <a-select v-model="query.type" placeholder="全部" allowClear>
+                <a-select-option :value="type.value" v-for="type in $enum.MACHINE_PROXY_TYPE" :key="type.value">
+                  {{type.label}}
+                </a-select-option>
+              </a-select>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="4">
+            <a-form-model-item>
+              <a-button-group>
+                <a-button type="primary" @click="getList({})" icon="search">查询</a-button>
+                <a-button @click="resetForm" icon="reload">重置</a-button>
+              </a-button-group>
+            </a-form-model-item>
+          </a-col>
+        </a-row>
+      </a-form-model>
+    </div>
+    <!-- 工具栏 -->
+    <div class="machine-proxy-tools-bar">
+      <a-button type="primary" icon="plus" @click="add">新建</a-button>
+      <a-button class="dynamic-button" type="danger" icon="delete"
+                v-show="selectedRowKeys.length" @click="batchRemove()">
+        批量删除
+      </a-button>
+    </div>
+    <!-- 表格 -->
+    <div class="machine-proxy-table">
+      <a-table :columns="columns"
+               :dataSource="rows"
+               :pagination="pagination"
+               :rowSelection="{selectedRowKeys, onChange: e => selectedRowKeys = e}"
+               rowKey="id"
+               @change="getList"
+               :loading="loading"
+               size="middle">
+        <!-- 主机 -->
+        <div slot="host" slot-scope="record">
+          <span>{{record.host}}</span>
+          <a class="copy-icon-right" @click="$copy(record.host)">
+            <a-icon type="copy"/>
+          </a>
+        </div>
+        <!-- 类型 -->
+        <a-tag slot="type" slot-scope="record" :color="$enum.valueOf($enum.MACHINE_PROXY_TYPE, record.type).color">
+          {{$enum.valueOf($enum.MACHINE_PROXY_TYPE, record.type).label}}
+        </a-tag>
+        <!-- 操作 -->
+        <div slot="action" slot-scope="record">
+          <!-- 修改 -->
+          <a @click="update(record)">修改</a>
+          <a-divider type="vertical"/>
+          <!-- 删除 -->
+          <a @click="remove(record.id)">删除</a>
+        </div>
+      </a-table>
+    </div>
+    <!-- 事件 -->
+    <div class="machine-proxy-event">
+      <AddMachineProxyModal ref="addModal" @added="getList({})" @updated="getList()"/>
+    </div>
+  </div>
+</template>
+
+<script>
+  import AddMachineProxyModal from '../../components/machine/AddMachineProxyModal'
+
+  /**
+   * 列
+   */
+  const columns = [
+    {
+      title: '序号',
+      key: 'seq',
+      customRender: (text, record, index) => `${index + 1}`,
+      width: 60,
+      align: 'center'
+    },
+    {
+      title: '代理主机',
+      key: 'host',
+      width: 200,
+      scopedSlots: { customRender: 'host' },
+      sorter: (a, b) => a.host.localeCompare(b.host)
+    },
+    {
+      title: '代理端口',
+      dataIndex: 'port',
+      key: 'port',
+      width: 150,
+      sorter: (a, b) => a.port > b.port
+    },
+    {
+      title: '代理用户',
+      dataIndex: 'username',
+      key: 'username',
+      width: 200,
+      sorter: (a, b) => a.host.localeCompare(b.host)
+    },
+    {
+      title: '代理类型',
+      key: 'type',
+      width: 150,
+      scopedSlots: { customRender: 'type' }
+    },
+    {
+      title: '描述',
+      dataIndex: 'description',
+      key: 'description',
+      ellipsis: true
+    },
+    {
+      title: '操作',
+      key: 'action',
+      fixed: 'right',
+      width: 160,
+      scopedSlots: { customRender: 'action' },
+      align: 'center'
+    }
+  ]
+
+  export default {
+    name: 'MachineProxy',
+    components: {
+      AddMachineProxyModal
+    },
+    data: function() {
+      return {
+        query: {
+          host: null,
+          port: null,
+          username: null,
+          type: null
+        },
+        loading: false,
+        rows: [],
+        selectedRowKeys: [],
+        pagination: {
+          current: 1,
+          pageSize: 10,
+          total: 0,
+          showTotal: function(total) {
+            return `共 ${total}条`
+          }
+        },
+        columns
+      }
+    },
+    methods: {
+      getList(page = this.pagination) {
+        this.loading = true
+        this.$api.getMachineProxyList({
+          ...this.query,
+          page: page.current,
+          limit: page.pageSize
+        }).then(({ data }) => {
+          const pagination = { ...this.pagination }
+          pagination.total = data.total
+          pagination.current = data.page
+          this.rows = data.rows
+          this.pagination = pagination
+          this.loading = false
+        }).catch(() => {
+          this.loading = false
+        })
+      },
+      add() {
+        this.$refs.addModal.add()
+      },
+      update(record) {
+        this.$refs.addModal.update(record)
+      },
+      remove(id) {
+        this.$confirm({
+          title: '确认删除',
+          content: '是否删除当前代理? 将会清除是所有关联机器的代理!',
+          okText: '确认',
+          okType: 'danger',
+          cancelText: '取消',
+          onOk: () => {
+            this.$api.deleteMachineProxy({
+              idList: [id]
+            }).then(() => {
+              this.$message.success('删除成功')
+              this.getList()
+            })
+          }
+        })
+      },
+      batchRemove() {
+        this.$confirm({
+          title: '确认删除',
+          content: '是否删除选中代理? 将会清除是所有关联机器的代理!',
+          okText: '确认',
+          okType: 'danger',
+          cancelText: '取消',
+          onOk: () => {
+            this.$api.deleteMachineProxy({
+              idList: this.selectedRowKeys
+            }).then(() => {
+              this.$message.success('删除成功')
+              this.getList()
+            })
+            this.selectedRowKeys = []
+          }
+        })
+      },
+      resetForm() {
+        this.$refs.query.resetFields()
+        this.getList({})
+      }
+    },
+    mounted() {
+      this.getList({})
+    }
+  }
+
+</script>
+
+<style scoped>
+
+  .machine-proxy-column {
+    margin: 12px 12px 0 12px;
+  }
+
+  .machine-proxy-tools-bar {
+    margin-bottom: 12px;
+  }
+
+  .machine-proxy-search-form /deep/ .ant-form-item {
+    display: flex;
+  }
+
+  .machine-proxy-search-form /deep/ .ant-form-item-control-wrapper {
+    flex: 0.8;
+  }
+
+</style>
