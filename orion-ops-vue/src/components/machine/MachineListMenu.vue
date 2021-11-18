752bf1472bceff932eb9a47ad36b4080e4932d9b
lijiahang <ljh1553488six@139.com>
2021-11-18 10:05:08 +0800

    feat: add machine sftp views.

---
+++ orion-ops-vue/src/components/machine/MachineListMenu.vue
@@ -0,0 +1,100 @@
+<template>
+  <a-layout-sider id="machine-list-fixed-left">
+    <a-spin :spinning="loading">
+      <!-- 工具按钮 -->
+      <div class="machine-tools">
+        <div class="machine-tools-item" v-if="!hideBack" title="返回" @click="backHistory">
+          <a-icon type="arrow-left"/>
+        </div>
+        <div class="machine-tools-item" title="刷新" @click="getMachineList">
+          <a-icon type="reload"/>
+        </div>
+      </div>
+      <!-- 列表菜单 -->
+      <a-menu theme="dark" mode="inline" :defaultSelectedKeys="selectedMachine">
+        <a-menu-item v-for="item of list" :key="item.id" :title="item.host" @click="$emit('chooseMachine', item.id)">
+          <a-icon type="desktop"/>
+          <span class="nav-text">{{ item.name }}</span>
+        </a-menu-item>
+      </a-menu>
+    </a-spin>
+  </a-layout-sider>
+</template>
+
+<script>
+export default {
+  name: 'MachineListMenu',
+  props: {
+    selectedMachine: Array,
+    hideBack: Boolean,
+    query: Object
+  },
+  data() {
+    return {
+      loading: false,
+      list: []
+    }
+  },
+  methods: {
+    backHistory() {
+      this.$router.back(-1)
+    },
+    async getMachineList() {
+      this.loading = true
+      try {
+        const machineListRes = await this.$api.getMachineList({
+          ...this.query,
+          limit: 10000
+        })
+        this.list = machineListRes.data.rows.map(i => {
+          return {
+            id: i.id,
+            tag: i.tag,
+            name: i.name,
+            host: i.host
+          }
+        })
+      } catch (e) {
+        // ignore
+      }
+      this.loading = false
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+#machine-list-fixed-left {
+  overflow: auto;
+
+  .machine-tools {
+    height: 32px;
+    margin: 8px 8px 4px 8px;
+    display: flex;
+    background: rgba(255, 255, 255, 0.2);
+    border-radius: 4px;
+
+    .machine-tools-item {
+      width: 100%;
+      height: 100%;
+      color: hsla(0, 0%, 100%, 0.65);
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      transition: color 0.3s ease-in-out, background-color 0.3s ease-in-out;
+      border-radius: 4px;
+      cursor: pointer;
+
+      i {
+        font-size: 20px;
+      }
+    }
+
+    .machine-tools-item:hover {
+      color: hsla(0, 0%, 100%, .2);
+      background-color: #1890ff;
+      color: #fff;
+    }
+  }
+}
+</style>
---
+++ orion-ops-vue/src/views/machine/MachineSftp.vue
@@ -0,0 +1,123 @@
+<template>
+  <a-layout id="sftp-layout-container">
+    <!-- 机器列表 -->
+    <MachineListMenu ref="machineList" :selectedMachine="selectedMachine" @chooseMachine="changeSftpMain"/>
+    <!-- main -->
+    <a-layout>
+      <a-layout-content id="sftp-content-fixed-right">
+        <!-- 面包屑 -->
+        <a-breadcrumb id="sftp-breadcrumb">
+          <a-breadcrumb-item>
+            <a-tag color="#7950F2" style="margin: 0">
+              {{ currentMachine.name }}
+            </a-tag>
+          </a-breadcrumb-item>
+          <a-breadcrumb-item>
+            <a-tag color="#5C7CFA" style="margin: 0">
+              {{ currentMachine.host }}
+            </a-tag>
+          </a-breadcrumb-item>
+          <a-breadcrumb-item>
+            <a-tag color="#20C997" style="margin: 0">
+              {{ currentMachine.tag }}
+            </a-tag>
+          </a-breadcrumb-item>
+        </a-breadcrumb>
+        <!-- sftp -->
+        <MachineSftpMain ref="sftpMain"
+                         :machineId="machineId"
+                         :leftFolderDefaultVisible="true"
+                         @opened="sftpOpened"/>
+      </a-layout-content>
+    </a-layout>
+  </a-layout>
+</template>
+
+<script>
+import MachineListMenu from '@/components/machine/MachineListMenu'
+import MachineSftpMain from '@/components/sftp/MachineSftpMain'
+
+export default {
+  name: 'MachineSftp',
+  components: {
+    MachineSftpMain,
+    MachineListMenu
+  },
+  data() {
+    return {
+      selectedMachine: [],
+      currentMachine: {
+        id: null,
+        tag: null,
+        name: null,
+        host: null
+      },
+      machineSessionToken: [],
+      machineId: null
+    }
+  },
+  methods: {
+    changeSftpMain(id) {
+      const filterMachines = this.$refs.machineList.list.filter(m => m.id === id)
+      if (filterMachines.length) {
+        const filterMachine = filterMachines[0]
+        this.currentMachine.id = filterMachine.id
+        this.currentMachine.tag = filterMachine.tag
+        this.currentMachine.name = filterMachine.name
+        this.currentMachine.host = filterMachine.host
+      }
+      this.machineId = id
+      const filterSessionList = this.machineSessionToken.filter(m => m.machineId === id)
+      if (filterSessionList.length) {
+        this.$refs.sftpMain.changeToken(filterSessionList[0].session)
+      } else {
+        this.$refs.sftpMain.openSftp()
+      }
+      this.$refs.sftpMain.cleanChooseTree()
+    },
+    sftpOpened(machineId, session) {
+      this.machineSessionToken.push({
+        machineId,
+        session
+      })
+    }
+  },
+  created() {
+    if (this.$route.params.id) {
+      this.machineId = parseInt(this.$route.params.id)
+    } else {
+      this.machineId = 1
+    }
+    this.selectedMachine = [this.machineId]
+  },
+  async mounted() {
+    await this.$refs.machineList.getMachineList()
+    this.changeSftpMain(this.machineId)
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+#sftp-layout-container {
+  height: 100vh;
+}
+
+#sftp-content-fixed-right {
+  overflow: auto;
+  padding: 18px;
+
+  #sftp-breadcrumb {
+    margin: 0 0 16px 0;
+    background-color: #FFF;
+    padding: 8px;
+    border-radius: 4px;
+  }
+}
+
+/deep/ .sftp-folder-left-fixed {
+  height: calc(100vh - 122px);
+  max-height: calc(100vh - 122px);
+}
+
+</style>
