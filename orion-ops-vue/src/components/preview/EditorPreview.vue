03c146d5b4da4044efe8041fdde2390b44964db0
lijiahang <ljh1553488six@139.com>
2021-11-19 13:54:28 +0800

    add editor preview.

---
+++ orion-ops-vue/src/components/preview/EditorPreview.vue
@@ -0,0 +1,44 @@
+<template>
+  <a-modal v-model="visible" :width="650">
+    <!-- 编辑器 -->
+    <Editor :value="value" :height="350" :readOnly="true"/>
+    <!-- 标题 -->
+    <div slot="title">
+      <span>预览</span>
+      <a @click="$copy(value)">
+        <a-icon class="copy-icon-right" type="copy"/>
+      </a>
+    </div>
+    <!-- 页脚 -->
+    <div slot="footer">
+      <a-button @click="() => visible = false">关闭</a-button>
+    </div>
+  </a-modal>
+</template>
+
+<script>
+import Editor from '@/components/editor/Editor'
+
+export default {
+  name: 'EditorPreview',
+  components: {
+    Editor
+  },
+  data() {
+    return {
+      visible: false,
+      value: null
+    }
+  },
+  methods: {
+    preview(value) {
+      this.visible = true
+      this.value = value
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
