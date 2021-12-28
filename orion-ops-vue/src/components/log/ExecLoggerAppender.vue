96a0e0e7dbfb52d74587d638d27265b1ff86d9a0
lijiahang <ljh1553488six@139.com>
2021-12-28 11:43:33 +0800

    add exec log modal.

---
+++ orion-ops-vue/src/components/log/ExecLoggerAppender.vue
@@ -0,0 +1,164 @@
+<template>
+  <div class="exec-logger-appender">
+    <LogAppender ref="appender"
+                 size="default"
+                 :appendStyle="{height: appenderHeight}"
+                 :relId="execId"
+                 :config="{type: $enum.FILE_TAIL_TYPE.EXEC_LOG.value, relId: execId}">
+      <!-- 左侧工具栏 -->
+      <div class="appender-left-tools" slot="left-tools">
+        <!-- 命令输入 -->
+        <a-input-search class="command-write-input"
+                        size="default"
+                        v-if="$enum.BATCH_EXEC_STATUS.RUNNABLE.value === status"
+                        v-model="command"
+                        placeholder="输入"
+                        @search="sendCommand">
+          <a-icon type="forward" slot="enterButton"/>
+        </a-input-search>
+        <!-- 状态 -->
+        <a-tag class="machine-exec-status" v-if="status" :color="$enum.valueOf($enum.BATCH_EXEC_STATUS, status).color">
+          {{ $enum.valueOf($enum.BATCH_EXEC_STATUS, status).label }}
+        </a-tag>
+        <!-- used -->
+        <span class="mx8" title="用时"
+              v-if="$enum.BATCH_EXEC_STATUS.COMPLETE.value === status">
+            {{ `${keepTime} (${used}ms)` }}
+        </span>
+        <!-- exitCode -->
+        <span class="mx8" title="退出码"
+              v-if="exitCode !== null"
+              :style="{'color': exitCode === 0 ? '#4263EB' : '#E03131'}">
+          {{ exitCode }}
+        </span>
+        <!-- 终止 -->
+        <a-button class="terminated-button"
+                  v-if="$enum.BATCH_EXEC_STATUS.RUNNABLE.value === status"
+                  icon="close"
+                  @click="terminated">
+          终止
+        </a-button>
+      </div>
+    </LogAppender>
+  </div>
+</template>
+
+<script>
+import LogAppender from './LogAppender'
+
+export default {
+  name: 'ExecLoggerAppender',
+  components: {
+    LogAppender
+  },
+  props: {
+    appenderHeight: {
+      type: String,
+      default: 'calc(100vh - 54px)'
+    }
+  },
+  data() {
+    return {
+      status: null,
+      execId: null,
+      command: null,
+      keepTime: null,
+      used: null,
+      exitCode: null,
+      pollId: null
+    }
+  },
+  methods: {
+    async open(execId) {
+      this.execId = execId
+      // 关闭轮询
+      if (this.pollId) {
+        clearInterval(this.pollId)
+      }
+      // 打开日志
+      this.$refs.appender.openTail()
+      // 获取状态
+      await this.getStatus()
+      // 检查轮询状态
+      if (this.status === this.$enum.BATCH_EXEC_STATUS.WAITING.value ||
+        this.status === this.$enum.BATCH_EXEC_STATUS.RUNNABLE.value) {
+        // 轮询状态
+        this.pollId = setInterval(this.pollStatus, 1000)
+      }
+    },
+    close() {
+      // 关闭tail
+      this.$refs.appender.clear()
+      this.$refs.appender.close()
+      // 关闭轮询
+      if (this.pollId) {
+        clearInterval(this.pollId)
+      }
+      this.status = null
+      this.execId = null
+      this.command = null
+      this.keepTime = null
+      this.used = null
+      this.exitCode = null
+      this.pollId = null
+    },
+    sendCommand() {
+      if (!this.command) {
+        return
+      }
+      const command = this.command
+      this.command = ''
+      this.$api.writeExecTask({
+        id: this.execId,
+        command
+      })
+    },
+    terminated() {
+      this.status = this.$enum.BATCH_EXEC_STATUS.TERMINATED.value
+      this.$api.terminatedExecTask({
+        id: this.execId
+      }).then(() => {
+        this.$message.success('已终止')
+      })
+    },
+    async getStatus() {
+      await this.$api.getExecTaskStatus({
+        idList: [this.execId]
+      }).then(({ data }) => {
+        if (data && data.length) {
+          const status = data[0]
+          this.status = status.status
+          this.exitCode = status.exitCode
+          this.used = status.used
+          this.keepTime = status.keepTime
+        }
+      })
+    },
+    async pollStatus() {
+      await this.getStatus()
+      if (this.status !== this.$enum.BATCH_EXEC_STATUS.WAITING.value &&
+        this.status !== this.$enum.BATCH_EXEC_STATUS.RUNNABLE.value) {
+        clearInterval(this.pollId)
+      }
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+.exec-logger-appender {
+  padding: 0 8px 8px 8px;
+
+  .appender-left-tools {
+    display: flex;
+    align-items: center;
+
+    .command-write-input {
+      margin-right: 8px;
+      width: 70%;
+    }
+  }
+}
+
+</style>
---
+++ orion-ops-vue/src/components/log/ExecLoggerAppenderModal.vue
@@ -0,0 +1,48 @@
+<template>
+  <a-modal v-model="visible"
+           :closable="false"
+           :title="null"
+           :footer="null"
+           :dialogStyle="{top: '16px'}"
+           :bodyStyle="{padding: '8px'}"
+           @cancel="close"
+           width="90%">
+    <!-- 日志面板 -->
+    <ExecLoggerAppender ref="logger" appenderHeight="calc(100vh - 110px)"/>
+  </a-modal>
+</template>
+
+<script>
+
+import ExecLoggerAppender from './ExecLoggerAppender'
+
+export default {
+  name: 'ExecLoggerAppenderModal',
+  components: {
+    ExecLoggerAppender
+  },
+  data() {
+    return {
+      visible: false
+    }
+  },
+  methods: {
+    open(id) {
+      this.visible = true
+      this.$nextTick(() => {
+        this.$refs.logger.open(id)
+      })
+    },
+    close() {
+      this.visible = true
+      this.$nextTick(() => {
+        this.$refs.logger.close()
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
