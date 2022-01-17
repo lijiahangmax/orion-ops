a7dc4ac2addfd88de789ec1770e6d82b4b7f09c3
lijiahang <ljh1553488six@139.com>
2022-01-17 17:01:47 +0800

    add release session holder.

---
+++ orion-ops-service/src/main/java/com/orion/ops/handler/release/ReleaseSessionHolder.java
@@ -0,0 +1,51 @@
+package com.orion.ops.handler.release;
+
+import com.orion.utils.collect.Maps;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * release 实例持有者
+ *
+ * @author Jiahang Li
+ * @version 1.0.0
+ * @since 2021/12/31 9:55
+ */
+@Component
+public class ReleaseSessionHolder {
+
+    /**
+     * session
+     */
+    private ConcurrentHashMap<Long, IReleaseProcessor> session = Maps.newCurrentHashMap();
+
+    /**
+     * 添加 session
+     *
+     * @param processor processor
+     */
+    public void addSession(IReleaseProcessor processor) {
+        session.put(processor.getReleaseId(), processor);
+    }
+
+    /**
+     * 获取 session
+     *
+     * @param id id
+     * @return session
+     */
+    public IReleaseProcessor getSession(Long id) {
+        return session.get(id);
+    }
+
+    /**
+     * 移除 session
+     *
+     * @param id id
+     */
+    public void removeSession(Long id) {
+        session.remove(id);
+    }
+
+}
