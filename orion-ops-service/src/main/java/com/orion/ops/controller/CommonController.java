6c85415421ce8348c33137ddbdda3785c0eac6d5
lijiahang <ljh1553488six@139.com>
2022-06-10 09:18:22 +0800

    fixed: machine import init env.

--- orion-ops-service/src/main/java/com/orion/ops/controller/CommonController.java
+++ orion-ops-service/src/main/java/com/orion/ops/controller/CommonController.java
@@ -3,14 +3,17 @@ package com.orion.ops.controller;
 import com.alibaba.fastjson.JSON;
 import com.orion.ops.OrionOpsServiceApplication;
 import com.orion.ops.annotation.RestWrapper;
+import com.orion.ops.consts.Const;
 import com.orion.ops.consts.user.RoleType;
 import com.orion.ops.entity.dto.UserDTO;
+import com.orion.ops.entity.vo.SystemVersionVO;
 import com.orion.ops.service.api.CommonService;
 import com.orion.ops.utils.Currents;
 import com.orion.utils.Exceptions;
 import com.orion.utils.io.StreamReaders;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,6 +55,15 @@ public class CommonController {
         }
         InputStream menu = OrionOpsServiceApplication.class.getResourceAsStream("/menu/" + menuFile);
         return JSON.parseArray(new String(StreamReaders.readAllBytes(menu)));
+    }
+
+    @GetMapping("/version")
+    @ApiOperation(value = "获取系统版本")
+    public SystemVersionVO getVersion() {
+        return SystemVersionVO.builder()
+                .orionKitVersion(Const.ORION_VERSION)
+                .orionOpsVersion(Const.ORION_OPS_VERSION)
+                .build();
     }
 
 }
