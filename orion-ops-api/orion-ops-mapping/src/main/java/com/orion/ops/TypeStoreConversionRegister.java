84b9992bd730694c3af9ec24f932c76156dacfec
lijiahang <ljh1553488six@139.com>
2022-08-11 18:03:02 +0800

    modify pom.xml.

--- orion-ops-api/orion-ops-mapping/src/main/java/com/orion/ops/TypeStoreConversionRegister.java
+++ orion-ops-api/orion-ops-mapping/src/main/java/com/orion/ops/TypeStoreConversionRegister.java
@@ -2,7 +2,9 @@ package com.orion.ops;
 
 import com.orion.lang.support.Attempt;
 import com.orion.lang.utils.reflect.PackageScanner;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
 
 /**
  * 类型转换注册器
@@ -11,18 +13,20 @@ import org.springframework.beans.factory.InitializingBean;
  * @version 1.0.0
  * @since 2022/8/10 17:23
  */
+@Component
+@Slf4j
 public class TypeStoreConversionRegister implements InitializingBean {
 
     private final String PACKAGE = this.getClass().getPackage().getName() + ".mapping.*";
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        // todo 测试
         new PackageScanner(PACKAGE)
                 .with(TypeStoreConversionRegister.class)
                 .scan()
                 .getClasses()
                 .forEach(Attempt.rethrows(s -> {
+                    log.info("register type conversion {}", s.getName());
                     Class.forName(s.getName());
                 }));
     }
