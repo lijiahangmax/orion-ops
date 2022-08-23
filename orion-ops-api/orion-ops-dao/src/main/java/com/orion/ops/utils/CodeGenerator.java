cbee7fd8ab146c9e353480442970695f20a2e935
lijiahang <ljh1553488six@139.com>
2022-08-23 17:35:58 +0800

    generator webhook code.

--- orion-ops-api/orion-ops-dao/src/main/java/com/orion/ops/utils/CodeGenerator.java
+++ orion-ops-api/orion-ops-dao/src/main/java/com/orion/ops/utils/CodeGenerator.java
@@ -112,7 +112,7 @@ public class CodeGenerator {
                 // 配置字段前缀
                 .setFieldPrefix("")
                 // 生成的表
-                .setInclude("machine_monitor");
+                .setInclude("webhook_config");
 
         // 包名策略配置
         PackageConfig pkConfig = new PackageConfig()
