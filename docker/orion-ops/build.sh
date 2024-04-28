22cafa10924377cf015cb400d4fbcf1b7a2a50a3
lijiahangmax <ljh1553488six@139.com>
2024-04-29 01:08:39 +0800

    update build script.

---
+++ docker/orion-ops/build.sh
@@ -0,0 +1,5 @@
+mv ../../orion-ops-api/orion-ops-web/target/orion-ops-web-1.2.3.jar ./
+mv ../../orion-ops-vue/dist ./
+docker build -t orion-ops:1.2.3 .
+rm -f ./orion-ops-web-1.2.3.jar
+rm -f ../../orion-ops-vue/dist ./dist
