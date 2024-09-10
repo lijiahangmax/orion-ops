a30397116953a1616965d4c343c12f2fa7108a8a
lijiahangmax <ljh1553488six@139.com>
2024-09-10 23:41:54 +0800

    :zap: 修改启动脚本.

--- orion-ops-api/orion-ops-service/src/main/resources/templates/script/start-monitor-agent.sh
+++ orion-ops-api/orion-ops-service/src/main/resources/templates/script/start-monitor-agent.sh
@@ -1,15 +1,28 @@
+#!/bin/bash
 AGENT_PROCESS=${processName}
-STARTED=`ps -ef | grep $AGENT_PROCESS | grep '.jar' | grep -v grep | awk '{print $2}' | wc -l`
-PID=`ps -ef | grep $AGENT_PROCESS | grep '.jar' | grep -v grep | awk '{print $2}'`
+STARTED=$(ps -ef | grep $AGENT_PROCESS | grep '.jar' | grep -v grep | wc -l)
+PIDS=$(ps -ef | grep $AGENT_PROCESS | grep '.jar' | grep -v grep | awk '{print $2}')
+
 # KILL
 if [ $STARTED -eq 0 ]
 then
-    echo "agent not started"
+    echo "Agent is not running."
 else
-    echo "kill pid $PID"
-	kill -9 $PID
+    echo "Killing agent with PID(s): $PIDS"
+    for PID in $PIDS; do
+        kill -9 $PID
+    done
 fi
-echo 'starting ' $AGENT_PROCESS '...'
+echo 'Agent starting...'
+
 # START
 nohup java -jar -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=56m -Xms128m -Xmx128m -Xmn32m -Xss256k -XX:SurvivorRatio=8 -XX:+UseConcMarkSweepGC ${agentJarPath} --machineId=${machineId} --spring.profiles.active=prod >/dev/null 2>&1 &
-echo 'agent started result: ' $?
+
+# CHECK
+sleep 2
+NEW_STARTED=$(ps -ef | grep $AGENT_PROCESS | grep '.jar' | grep -v grep | wc -l)
+if [ $NEW_STARTED -eq 0 ]; then
+    echo "Failed to start agent."
+else
+    echo "Agent started successfully."
+fi
