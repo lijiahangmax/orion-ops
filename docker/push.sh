c05504d71d6648bc34a0a44bd5ae381731212090
lijiahang <ljh1553488six@139.com>
2024-10-24 09:37:23 +0800

    :bookmark: 升级版本.

--- docker/push.sh
+++ docker/push.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-version=1.2.8
+version=1.2.9
 docker push registry.cn-hangzhou.aliyuncs.com/lijiahangmax/orion-ops-adminer:${version}
 docker push registry.cn-hangzhou.aliyuncs.com/lijiahangmax/orion-ops-mysql:${version}
 docker push registry.cn-hangzhou.aliyuncs.com/lijiahangmax/orion-ops-redis:${version}
